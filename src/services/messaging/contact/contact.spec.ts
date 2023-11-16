238f8d3b8342db7682533d845b28825f105cfac7
peoray <peoray@yahoo.com>
2023-11-16 04:10:26 +0100

    test: Add test case for creating contact

--- src/services/messaging/contact/contact.spec.ts
+++ src/services/messaging/contact/contact.spec.ts
@@ -38,12 +38,45 @@ const fetchContactsResponseData: IFetchContactsResponse = {
   },
 }
 
+const payload = {
+  phone_number: '8123696237',
+  email_address: 'test@gmail.com',
+  first_name: 'test',
+  last_name: 'contact',
+  company: 'Termii',
+  country_code: '234',
+}
+
+const responseData = {
+  data: {
+    id: 3647982,
+    phone_number: '2448123696237',
+    email_address: 'test@gmail.com',
+    message: null,
+    company: 'Termii',
+    first_name: 'test',
+    last_name: 'contact',
+    create_at: '2021-10-11 10:15:35',
+    updated_at: '2021-10-11 10:15:35',
+  },
+}
+
 describe('Contact', () => {
   it('should fetch list of contacts', async () => {
     try {
       const response = await contactInstance.fetchContacts('123456')
 
       expect(response).toEqual(fetchContactsResponseData)
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error)
+    }
+  })
+
+  it('should create a contact', async () => {
+    try {
+      const response = await contactInstance.createContact('12345678', payload)
+
+      expect(response).toEqual(responseData)
     } catch (error) {
       expect(error).toBeInstanceOf(Error)
     }
