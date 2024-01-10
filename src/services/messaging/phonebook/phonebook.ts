bc5cfcfd3253c34537898da153cb48764c346745
peoray <peoray@yahoo.com>
2024-01-10 13:34:12 +0100

    refactor: Remove data from request object and add jsdoc to phonebook handler

--- src/services/messaging/phonebook/phonebook.ts
+++ src/services/messaging/phonebook/phonebook.ts
@@ -3,24 +3,35 @@ import {
   IFetchPhonebooksResponse,
   IPhonebook,
   IPhonebookResponse,
-  //   IUpdatePhonebook,
-  //   IUpdatePhonebookResponse,
 } from '../../../types'
 import { IAxiosStruct, handleErrors } from '../../../utils'
 
+/**
+ * Represents the Phonebook class, extending TermiiCore, to interact with the Termii API for phonebook-related functionalities.
+ * @extends TermiiCore
+ */
 export class Phonebook extends TermiiCore {
+  /**
+   * Creates an instance of Phonebook.
+   * @constructor
+   * @param {string} apiKey - The API key used for authorization.
+   */
   constructor(apiKey: string) {
     super(apiKey)
   }
 
+  /**
+   * Fetches a list of phonebooks.
+   * @param {number} [page] - The page number for paginated results (optional).
+   * @returns {Promise<IFetchPhonebooksResponse>} A promise that resolves with the list of phonebooks or rejects with an error.
+   */
   public async fetchPhonebooks(
     page?: number
   ): Promise<IFetchPhonebooksResponse> {
     try {
       const requestObj: IAxiosStruct = {
         method: 'GET',
-        url: `/phonebooks`,
-        page,
+        url: `/phonebooks?page=${page}`,
       }
 
       const response = await this.useRequest(requestObj)
@@ -31,6 +42,11 @@ export class Phonebook extends TermiiCore {
     }
   }
 
+  /**
+   * Creates a new phonebook.
+   * @param {IPhonebook} data - The payload containing the necessary information for creating the phonebook.
+   * @returns {Promise<IPhonebookResponse>} A promise that resolves with the response from creating the phonebook or rejects with an error.
+   */
   public async createPhonebook(data: IPhonebook): Promise<IPhonebookResponse> {
     try {
       const requestObj: IAxiosStruct = {
@@ -47,6 +63,12 @@ export class Phonebook extends TermiiCore {
     }
   }
 
+  /**
+   * Updates an existing phonebook.
+   * @param {string} phonebook_id - The ID of the phonebook to update.
+   * @param {IPhonebook} data - The payload containing the necessary information for updating the phonebook.
+   * @returns {Promise<IPhonebookResponse>} A promise that resolves with the response from updating the phonebook or rejects with an error.
+   */
   public async updatePhonebook(
     phonebook_id: string,
     data: IPhonebook
@@ -66,6 +88,11 @@ export class Phonebook extends TermiiCore {
     }
   }
 
+  /**
+   * Deletes a phonebook.
+   * @param {string} phonebook_id - The ID of the phonebook to delete.
+   * @returns {Promise<IPhonebookResponse>} A promise that resolves with the response from deleting the phonebook or rejects with an error.
+   */
   public async deletePhonebook(
     phonebook_id: string
   ): Promise<IPhonebookResponse> {
