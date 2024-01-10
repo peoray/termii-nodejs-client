8ad44ba27f98056a0bf5403f5f5b81cffae4f201
peoray <peoray@yahoo.com>
2024-01-10 13:13:21 +0100

    refactor: Remove data from request object and add jsdoc to contact handler

--- src/services/messaging/contact/contact.ts
+++ src/services/messaging/contact/contact.ts
@@ -7,11 +7,26 @@ import {
 } from '../../../types'
 import { IAxiosStruct, handleErrors } from '../../../utils'
 
+/**
+ * Represents the Contact class, extending TermiiCore, to interact with the Termii API for contact-related functionalities.
+ * @extends TermiiCore
+ */
 export class Contact extends TermiiCore {
+  /**
+   * Creates an instance of Contact.
+   * @constructor
+   * @param {string} apiKey - The API key used for authorization.
+   */
   constructor(apiKey: string) {
     super(apiKey)
   }
 
+  /**
+   * Fetches the contacts for a specific phonebook.
+   * @param {string} phonebook_id - The ID of the phonebook to fetch contacts from.
+   * @param {number} [page] - The page number for paginated results (optional).
+   * @returns {Promise<IFetchContactsResponse>} A promise that resolves with the list of contacts or rejects with an error.
+   */
   public async fetchContacts(
     phonebook_id: string,
     page?: number
@@ -19,8 +34,7 @@ export class Contact extends TermiiCore {
     try {
       const requestObj: IAxiosStruct = {
         method: 'GET',
-        url: `/phonebooks/${phonebook_id}/contacts`,
-        page,
+        url: `/phonebooks/${phonebook_id}/contacts?page=${page}`,
       }
 
       const response = await this.useRequest(requestObj)
@@ -31,6 +45,12 @@ export class Contact extends TermiiCore {
     }
   }
 
+  /**
+   * Creates a new contact in a specific phonebook.
+   * @param {string} phonebook_id - The ID of the phonebook to add the contact to.
+   * @param {ICreateContact} data - The payload containing the necessary information for creating the contact.
+   * @returns {Promise<ICreateContactResponse>} A promise that resolves with the response from creating the contact or rejects with an error.
+   */
   public async createContact(
     phonebook_id: string,
     data: ICreateContact
@@ -50,6 +70,11 @@ export class Contact extends TermiiCore {
     }
   }
 
+  /**
+   * Deletes a contact from the phonebook.
+   * @param {string} contact_id - The ID of the contact to delete.
+   * @returns {Promise<IDeleteContactResponse>} A promise that resolves with the response from deleting the contact or rejects with an error.
+   */
   public async deleteContact(
     contact_id: string
   ): Promise<IDeleteContactResponse> {
