3ada7827763238b83c1600f2ed309357c3854bd3
peoray <peoray@yahoo.com>
2024-01-10 13:05:13 +0100

    refactor: Remove data from request object and add jsdoc to campaign handler

--- src/services/messaging/campaign/campaign.ts
+++ src/services/messaging/campaign/campaign.ts
@@ -7,17 +7,30 @@ import {
 } from '../../../types'
 import { IAxiosStruct, handleErrors } from '../../../utils'
 
+/**
+ * Represents the Campaign class, extending TermiiCore, to interact with the Termii API for campaign-related functionalities.
+ * @extends TermiiCore
+ */
 export class Campaign extends TermiiCore {
+  /**
+   * Creates an instance of Campaign.
+   * @constructor
+   * @param {string} apiKey - The API key used for authorization.
+   */
   constructor(apiKey: string) {
     super(apiKey)
   }
 
+  /**
+   * Fetches a list of campaigns.
+   * @param {number} [page] - The page number for paginated results (optional).
+   * @returns {Promise<IFetchCampaignsResponse>} A promise that resolves with the list of campaigns or rejects with an error.
+   */
   public async fetchCampaigns(page?: number): Promise<IFetchCampaignsResponse> {
     try {
       const requestObj: IAxiosStruct = {
         method: 'GET',
-        url: `sms/campaigns`,
-        page,
+        url: `sms/campaigns?page=${page}`,
       }
 
       const response = await this.useRequest(requestObj)
@@ -28,6 +41,12 @@ export class Campaign extends TermiiCore {
     }
   }
 
+  /**
+   * Fetches the history of a specific campaign.
+   * @param {string} campaign_id - The ID of the campaign to fetch history for.
+   * @param {number} [page] - The page number for paginated results (optional).
+   * @returns {Promise<IFetchCampaignHistoryResponse>} A promise that resolves with the campaign history or rejects with an error.
+   */
   public async fetchCampaignHistory(
     campaign_id: string,
     page?: number
@@ -35,8 +54,7 @@ export class Campaign extends TermiiCore {
     try {
       const requestObj: IAxiosStruct = {
         method: 'GET',
-        url: `sms/campaigns/${campaign_id}`,
-        page,
+        url: `sms/campaigns/${campaign_id}?page=${page}`,
       }
 
       const response = await this.useRequest(requestObj)
@@ -47,6 +65,11 @@ export class Campaign extends TermiiCore {
     }
   }
 
+  /**
+   * Sends a campaign.
+   * @param {ISendCampaign} data - The payload containing the necessary information for sending the campaign.
+   * @returns {Promise<ISendCampaignResponse>} A promise that resolves with the response from sending the campaign or rejects with an error.
+   */
   public async sendCampaign(
     data: ISendCampaign
   ): Promise<ISendCampaignResponse> {
